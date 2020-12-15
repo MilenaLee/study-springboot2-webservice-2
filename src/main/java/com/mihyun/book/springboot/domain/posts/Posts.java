99c8861a20f8c88ca7c0dc20501a528290a8653d
mmhyun.lee@gmail.com <mmhyun.lee@gmail.com>
2020-12-15 22:02:29 +0900

    3.5 JPA Auditing으로 생성시간/수정시간 자동화하기

--- src/main/java/com/mihyun/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/mihyun/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.mihyun.book.springboot.domain.posts;
 
+import com.mihyun.book.springboot.domain.BaseTimeEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,4 +34,8 @@ public class Posts {
         this.author = author;
     }
 
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
